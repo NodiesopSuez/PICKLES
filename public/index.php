69107678258f0bcfb92a355186ba910e897348b5
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-05-20 14:13:04 +0900

    cousay用に追加したコードを一旦削除

--- public/index.php
+++ public/index.php
@@ -59,7 +59,3 @@ $response->send();
 
 $kernel->terminate($request, $response);
 
-$app->get('/cowsay', function() use($app) {
-    $app['monolog']->addDebug('cowsay');
-    return "<pre>".\Cowsayphp\Cow::say("Cool beans")."</pre>";
-  });
