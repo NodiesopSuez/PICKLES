4ced22f08a113d27c782bca9f1aa68de0aa35135
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-10 14:25:45 +0900

    l.11,assetをmixに変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,17 +2,19 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1 ">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <!-- Scripts -->
-    <script src="{{ secure_asset('js/app.js') }}" defer></script>
+    <!-- <script src="{{ secure_asset('js/app.js') }}" defer></script> -->
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
+    <!-- <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet"> -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     <!-- Favicon -->
     <link rel="icon" href="./favicon.ico" />
 </head>
