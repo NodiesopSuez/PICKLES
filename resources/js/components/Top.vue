1fe884905ee7cc081dadf005a283e479ec0b32ed
NodiesopSuez <ddw.suez.nodiesop@gmail.com>
2021-06-10 14:47:24 +0900

    背景色変更

--- resources/js/components/Top.vue
+++ resources/js/components/Top.vue
@@ -403,7 +403,7 @@ export default {
     width: 100%;
     height: 600px;
     overflow: hidden;
-    background: linear-gradient(to bottom, rgb(131, 130, 130) 53%, var(--for-background) 47% 100%) ;
+    background: linear-gradient(to bottom, #fff 53%, var(--for-background) 47% 100%) ;
 }
 
 .top_section button {
@@ -653,7 +653,7 @@ input[name="word"] {
         flex-direction: column;
         align-items: center;
         justify-content: space-between;
-        background:linear-gradient(to bottom, #ccc 50%, var(--for-background) 50% 100%);
+        background:linear-gradient(to bottom, #fff 50%, var(--for-background) 50% 100%);
     }
     
     .menu {
